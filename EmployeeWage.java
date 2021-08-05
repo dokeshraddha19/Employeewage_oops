f61f243c57a6d313fae2aae6953223f9bf87a8d1
dokeshraddha19 <shraddhadoke1999@gmail.com>
2021-08-05 19:08:40 +0530

    Welcome commit

---
+++ EmployeeWage.java
@@ -0,0 +1,20 @@
+package construct;
+import java.util.*;
+public class EmployeeWage {
+	public static void main(String[] args)
+	{
+	Employee emp=new Employee();
+	emp.welcome();
+	}
+}
+
+
+class Employee {
+	void welcome()
+
+	{
+	System.out.println("Welcome to Employee Wage Computation");
+	}
+}
+
+
