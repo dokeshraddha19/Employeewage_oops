5d5d337dc5e77f4c4683c05db6a2c2136172edb0
dokeshraddha19 <shraddhadoke1999@gmail.com>
2021-08-05 23:16:42 +0530

    UC2 commit

--- EmployeeWage.java
+++ EmployeeWage.java
@@ -4,17 +4,30 @@ public class EmployeeWage {
 	public static void main(String[] args)
 	{
 	Employee emp=new Employee();
-	emp.welcome();
+	emp.attendence();
+	emp.dailyWage();
 	}
 }
-
-
 class Employee {
-	void welcome()
+	void attendence() {
+		
+	int is_Full_Time=1;
+	int empCheck=(int)Math.floor(Math.random() *10) % 2;
 
+	if(empCheck== is_Full_Time)
+		System.out.println("Employee is Present");
+	else
+		System.out.println("Employee is Absent");
+}
+	void dailyWage()
 	{
-	System.out.println("Welcome to Employee Wage Computation");
+		int wage_Per_Hour=20;
+		int daily_Hours=8;
+		int dailyWage=wage_Per_Hour*daily_Hours;
+		System.out.println("Daily wage of Employee = " +dailyWage);
 	}
 }
+
+
 
 
